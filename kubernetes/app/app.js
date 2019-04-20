03d4cda2dc36f59895d86b88f4622ed3ce5e29fc
Felippe <mendonca.felippe@gmail.com>
2019-04-20 02:27:08 -0300

    health ckeck added to application - changes on app.js to do so - liveness added on deployment manifest

--- kubernetes/app/app.js
+++ kubernetes/app/app.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
+const router = express.Router()
 const port = 3000;
+
+var isHealthy = true
 
 app.listen(port);
 console.log(`Aplicação teste executando em http://localhost: ${port}`);
-app.get('/', (req, res) => {
+
+router.get('/', (req, res) => {
   const name = process.env.NAME || 'candidato';
   res.send(`Olá ${name}!`);
 });
+
+router.get('/healthz', (req, res) => {
+  res.statusCode = isHealthy ? 200 : 500;
+  res.send()
+});
+
+router.get('/make-unhealthy', (req, res) => {
+  isHealthy = false;
+  res.send("Service will pretend to be unhealthy on next health check.")
+});
+
+app.use(router)
